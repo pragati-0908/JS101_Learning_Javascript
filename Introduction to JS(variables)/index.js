563a17deeaa699d93ba19c31430225da7f19688f
pragati-0908 <pragsgupta09@gmail.com>
2022-11-05 20:08:56 +0530

    First commit

---
+++ Introduction to JS(variables)/index.js
@@ -0,0 +1,80 @@
+let a = ":||\\||<00>||\\||:<REPORT CARD>:||\\||<00>||\\||:";
+console.log(a);
+
+x="!~~~~~~~!\FIRST STUDENT/!~~~~~~~!";
+console.log(x);
+
+name = "Akshat Gupta";
+console.log(name);
+
+school = "Delhi Public School";
+console.log(school);
+
+grade = "class_2nd";
+section= "~~section_A~~";
+console.log(grade,section);
+
+xx="rollno-05";
+console.log(xx);
+
+subject1 = "English, marks:86/100";
+console.log(subject1);
+
+subject2 = "Hindi, marks:76/100";
+console.log(subject2);
+
+subject3 = "Mathematics, marks:90/100";
+console.log(subject3);
+
+
+let y="$$:::::\\\\\\\\\\**SECOND STUDENT**//////::::$$";
+console.log(y);
+
+
+name2nd ="Himanshu Mishra";
+console.log(name2nd);
+
+school2nd ="Delhi Public School";
+console.log(school2nd);
+
+grade2nd = "class_2nd";
+section2nd = "~~section_A~~";
+console.log(grade2nd,section2nd);
+
+yy="rollno-16";
+console.log(yy);
+
+subject11="English, marks:96/100";
+console.log(subject11);
+subject22="Hindi, marks:72/100";
+console.log(subject22);
+subject33="Mathematics, marks:89";
+console.log(subject33);
+
+let z="###:::::\\\\\\\**THIRD STUDENT**//////:::::###";
+console.log(z);
+
+name3="Purvi Sahu";
+console.log(name3);
+
+school3="Delhi Public School";
+console.log(school3);
+
+grade3="class_2nd";
+section3="~~section_A~~";
+
+console.log(grade3,section3);
+
+zz="rollno-09";
+
+
+console.log(zz);
+
+
+
+subject111="English, marks:91/100";
+console.log(subject111);
+subject222="Hindi, marks:87/100";
+console.log(subject222);
+subject333="Mathematics, marks:85/100";
+console.log(subject333);
